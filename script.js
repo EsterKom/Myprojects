af16fbdefebf986a80711e4f096f6890feddc97f
EsterKom <104313292+EsterKom@users.noreply.github.com>
2022-09-08 11:57:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("OK");
 
-let body = document.querySelector("body")
-body.innerHTML = "My Projects"
+let title = document.querySelector("title")
+title.innerHTML = "My Projects"
 
 let h1 = document.querySelector("h1")
 h1.innerHTML = "It is ok, it works!"
